8b33a8cfdfaec8b0414f7ced357de947cd93233d
lkukGP <lkuk@globalization-partners.com>
2022-12-08 00:40:07 +0100

    day5

---
+++ day5/part1.py
@@ -0,0 +1,53 @@
+import os
+
+
+def build_stack_struct(stacks):
+    indexes = [x.replace("\n", "") for x in stacks[8:9]][0]
+    stacks = [x.replace("\n", "") for x in stacks[0:8]]
+    stack_struct = []
+    for index, number in enumerate(indexes):
+        if not number.isnumeric():
+            continue
+        stack_struct.append([])
+        for stack in stacks:
+            if len(stack) > index:
+                value = stack[index]
+                if value.isalpha():
+                    stack_struct[int(number) - 1].insert(0, value)
+
+    return stack_struct
+
+
+def make_moves(stack, moves):
+    for move in moves:
+        operators = [int(s) for s in move.split() if s.isdigit()]
+        for count in range(0, operators[0]):
+            moved_value = stack[operators[1] - 1].pop()
+            stack[operators[2] - 1].append(moved_value)
+    return stack
+
+
+def calculate(lines):
+    stack = build_stack_struct(lines[0:9])
+    output_stack_struct = make_moves(stack, lines[10:])
+    output = ""
+    for stack in output_stack_struct:
+        output += stack.pop()
+
+    return output
+
+
+def read_input():
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(current_dir + "/input.txt", "r") as f:
+        return f.readlines()
+
+
+def main():
+    lines = read_input()
+    output = calculate(lines)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
