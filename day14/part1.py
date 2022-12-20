c45ecc73de7619b2535095ed777dd90d392c7150
lkukGP <lkuk@globalization-partners.com>
2022-12-20 22:23:37 +0100

    day 14 part 1

--- day14/part1.py
+++ day14/part1.py
@@ -1,7 +1,11 @@
 import os
+import sys
+sys.setrecursionlimit(25000)
+
 
 ROCK = "#"
 AIR = "."
+SAND_START = "+"
 SAND = "O"
 
 
@@ -13,6 +17,8 @@ class Board:
         self.max_x = -99999999
         self.min_y = 99999999
         self.max_y = -99999999
+        self.sand_starting_position = (0, 500)
+        self.rest = 0
 
     def read_pairs(self, pairs):
         previous_x = 0
@@ -45,11 +51,9 @@ class Board:
     def fill(self):
         for y in range(0, self.max_y + 1):
             self.board.append([])
-            for x in range(self.min_x, self.max_x + 1):
-                if (x, y) == (500, 0):
-                    self.board[y].append(SAND)
-                elif y < self.min_y:
-                    self.board[y].append(AIR)
+            for x in range(0, self.max_x + 1):
+                if (y, x) == self.sand_starting_position:
+                    self.board[y].append(SAND_START)
                 elif (x, y) in self.rock_points:
                     self.board[y].append(ROCK)
                 else:
@@ -57,9 +61,35 @@ class Board:
 
     def print(self):
         for y in range(0, self.max_y + 1):
-            for x in range(self.min_x, self.max_x + 1):
-                print(self.board[y][x - self.min_x], end="")
+            for x in range(self.min_x - 1, self.max_x + 1):
+                print(self.board[y][x], end="")
             print()
+
+    def fall_sand(self):
+        position = self.sand_starting_position
+        while True:
+            if len(self.board) - 2 < position[0]:
+                return
+
+            if self.board[position[0] + 1][position[1]] == AIR:
+                if position != self.sand_starting_position:
+                    self.board[position[0]][position[1]] = AIR
+                self.board[position[0] + 1][position[1]] = SAND
+                position = (position[0] + 1, position[1])
+            elif (position[1] - 1 > 0) and (self.board[position[0] + 1][position[1] - 1] == AIR):
+                if position != self.sand_starting_position:
+                    self.board[position[0]][position[1]] = AIR
+                self.board[position[0] + 1][position[1] - 1] = SAND
+                position = (position[0] + 1, position[1] - 1)
+            elif (position[1] + 1 < len(self.board[position[0] + 1])) and (
+                    self.board[position[0] + 1][position[1] + 1] == AIR):
+                if position != self.sand_starting_position:
+                    self.board[position[0]][position[1]] = AIR
+                self.board[position[0] + 1][position[1] + 1] = SAND
+                position = (position[0] + 1, position[1] + 1)
+            else:
+                self.rest += 1
+                position = self.sand_starting_position
 
     @staticmethod
     def create(lines):
@@ -73,16 +103,16 @@ class Board:
 
 
 def calculate(lines):
-    output = 0
     board = Board.create(lines)
+    board.fall_sand()
     board.print()
 
-    return output
+    return board.rest
 
 
 def read_input():
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(current_dir + "/input2.txt", "r") as f:
+    with open(current_dir + "/input.txt", "r") as f:
         return f.readlines()
 
 
